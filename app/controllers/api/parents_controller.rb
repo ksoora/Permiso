7ae1030909f8a99a837ac1c16d9b444095829664
Workstation1 <workstation1@Workstation1s-MacBook-Air.local>
2015-03-04 15:57:44 -0600

    changed parent and teacher controller for fornt ender access

--- app/controllers/api/parents_controller.rb
+++ app/controllers/api/parents_controller.rb
@@ -2,4 +2,14 @@ class Api::ParentsController < ApplicationController
   def index
     render json: Parent.all
   end
+
+  def create
+  render json: Parent.create!(
+    parent_email: params[:parent][:email],
+    parent_first_name: params[:parent][:first_name],
+    parent_last_name: params[:parent][:last_name],
+    parent_student_full_name: params[:parent][:student_full_name],
+    parent_health_directory: params[:parent][:health_dietary])
+  end
+
 end
--- app/controllers/api/teachers_controller.rb
+++ app/controllers/api/teachers_controller.rb
@@ -2,4 +2,14 @@ class Api::TeachersController < ApplicationController
   def index
     render json: Teacher.all
   end
+
+  def create
+  render json: Teacher.create!(
+    teacher_email: params[:teacher][:teacher_email],
+    teacher_first_name: params[:teacher][:first_name],
+    teacher_last_name: params[:teacher][:last_name],
+    teacher_school: params[:teacher][:school],
+    teacher_subject: params[:teacher][:subject])
+  end
+
 end
