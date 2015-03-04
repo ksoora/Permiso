eb0ee7432ba02317d038e34d84d59a53127bf014
Workstation1 <workstation1@Workstation1s-MacBook-Air.local>
2015-03-04 13:47:38 -0600

    add CORS

--- app/controllers/api/teachers_controller.rb
+++ app/controllers/api/teachers_controller.rb
@@ -2,4 +2,15 @@ class Api::TeachersController < ApplicationController
   def index
     render json: Teacher.all
   end
+
+   def show
+   render json: Teacher.find params[:id]
+   end
+
+   def create
+    @teacher = Teacher.find(params[:id])
+
+    @teacher = Teacher.new params.require(:email) (:first_name) (:last_name) (:school) (:subject)
+   end
+
 end
